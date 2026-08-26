8e3e019f9959b08850026df4912a6c3d22469c12
Fabricio Moyano <fabricx150@gmail.com>
2026-08-26 16:38:12 -0300

    Creada la estructura inicial del proyecto

---
+++ tests/Tetris.test.ts
@@ -0,0 +1,9 @@
+import { describe, expect, test } from "vitest";
+import { Tetris } from "../src/Tetris";
+
+describe("Tetris", () => {
+    test("Existe la clase tetris con pieza cuadrada", () => {
+    const t = new Tetris("Cuadrado");
+    expect(t).toBe("Cuadrado");
+    })
+})
