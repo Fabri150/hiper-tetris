c57748d00ac1f75d6fbeec3190939df9a4fd92ab
amazingmanteca <caminosgerardoariel2016@gmail.com>
2026-08-30 15:58:06 -0300

    Añadido el atributo _forma a la clase Pieza para crear la forma especifica de cada pieza.

--- src/Pieza.ts
+++ src/Pieza.ts
@@ -3,7 +3,8 @@ import { IRotador } from "./IRotador";
 export abstract class Pieza implements IRotador {
 
     constructor(
-        private _nombre: string
+        private _nombre: string,
+        private _forma: number[][] = [],
     ) {}
 
     abstract rotarIzquierda(): void;
