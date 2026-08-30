9e68b311949da552ccef2621f5757154dcbd2e68
amazingmanteca <caminosgerardoariel2016@gmail.com>
2026-08-30 16:15:23 -0300

    Se cambio en Pieza.ts los atributos de private a protected, se implementaron los movimientos a derecha, izquierda y abajo, se les asigna su getter. En PiezaT.ts se heredan los atributos y se añadio el metodo de rotacion.

--- src/Pieza.ts
+++ src/Pieza.ts
@@ -1,11 +1,21 @@
 import { IRotador } from "./IRotador";
 
 export abstract class Pieza implements IRotador {
-
+    protected _x: number = 3;
+    protected _y: number = 0;
     constructor(
-        private _nombre: string,
-        private _forma: number[][] = [],
+        protected _nombre: string,
+        protected _forma: number[][] = [],
     ) {}
+
+    get forma() { return this._forma; }
+    get nombre() { return this._nombre; }
+    get x() { return this._x; }
+    get y() { return this._y; }
+
+    moverAbajo() { this._y++; }
+    moverIzquierda() { this._x--; }
+    moverDerecha() { this._x++; }
 
     abstract rotarIzquierda(): void;
     abstract rotarDerecha(): void;
--- src/PiezaT.ts
+++ src/PiezaT.ts
@@ -1,9 +1,39 @@
-import {Pieza} from "./Pieza";
+import { Pieza } from './Pieza';
 
 export class PiezaT extends Pieza {
-    constructor(
-        private _nombre: string = "T"
-    ) {
-        super(_nombre);
+    constructor() {
+        super('T', [
+            [0, 1, 0],
+            [1, 1, 1],
+            [0, 0, 0]
+        ]);
+    }
+
+    rotarDerecha(): void {
+        const n = this._forma.length;
+        const nuevaForma: number[][] = [];
+
+        for (let i = 0; i < n; i++) {
+            nuevaForma.push([]);
+            for (let j = 0; j < n; j++) {
+                nuevaForma[i][j] = this._forma[n - 1 - j][i];
+            }
+        }
+        
+        this._forma = nuevaForma;
+    }
+
+    rotarIzquierda(): void {
+        const n = this._forma.length;
+        const nuevaForma: number[][] = [];
+
+        for (let i = 0; i < n; i++) {
+            nuevaForma.push([]);
+            for (let j = 0; j < n; j++) {
+                nuevaForma[i][j] = this._forma[j][n - 1 - i];
+            }
+        }
+        
+        this._forma = nuevaForma;
     }
 }
