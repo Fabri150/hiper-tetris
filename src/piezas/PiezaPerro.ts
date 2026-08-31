21bef51906f456d613694b499d259e846dc0956d
Fabricio Moyano <fabricx150@gmail.com>
2026-08-31 17:30:56 -0300

    Se elimino el metodo rotacion de las piezas y ahora lo heredan de la clase Pieza

--- src/piezas/PiezaPerro.ts
+++ src/piezas/PiezaPerro.ts
@@ -1,42 +1,22 @@
 import { Pieza } from "./Pieza";
 
+type LadoPerro = "izquierda" | "derecha";
+
 export class PiezaPerro extends Pieza {
-
-    constructor() {
-        super("Perro", [
-            [0, 1, 1],
-            [1, 1, 0],
-            [0, 0, 0]
-        ]);
-    }
-
-    rotarDerecha(): void {
-        const n = this._forma.length;
-        const nuevaForma: number[][] = [];
-
-        for (let columna = 0; columna < n; columna++) {
-            nuevaForma.push([]);
-
-            for (let fila = 0; fila < n; fila++) {
-                nuevaForma[columna][fila] = this._forma[n - 1 - fila][columna];
-            }
-        }
-
-        this._forma = nuevaForma;
-    }
-
-    rotarIzquierda(): void {
-        const n = this._forma.length;
-        const nuevaForma: number[][] = [];
-
-        for (let columna = 0; columna < n; columna++) {
-            nuevaForma.push([]);
-
-            for (let fila = 0; fila < n; fila++) {
-                nuevaForma[columna][fila] = this._forma[fila][n - 1 - columna];
-            }
-        }
-
-        this._forma = nuevaForma;
+    constructor(lado: LadoPerro = "izquierda") {
+        super(
+            `Perro ${lado}`,
+            lado === "izquierda"
+                ? [
+                    [0, 1, 1],
+                    [1, 1, 0],
+                    [0, 0, 0]
+                ]
+                : [
+                    [1, 1, 0],
+                    [0, 1, 1],
+                    [0, 0, 0]
+                ]
+        );
     }
 }
--- src/piezas/PiezaT.ts
+++ src/piezas/PiezaT.ts
@@ -1,39 +1,11 @@
-import { Pieza } from './Pieza';
+import { Pieza } from "./Pieza";
 
 export class PiezaT extends Pieza {
     constructor() {
-        super('T', [
+        super("T", [
             [0, 1, 0],
             [1, 1, 1],
             [0, 0, 0]
         ]);
-    }
-
-    rotarDerecha(): void {
-        const n = this._forma.length;
-        const nuevaForma: number[][] = [];
-
-        for (let columna = 0; columna < n; columna++) {
-            nuevaForma.push([]);
-            for (let fila = 0; fila < n; fila++) {
-                nuevaForma[columna][fila] = this._forma[n - 1 - fila][columna];
-            }
-        }
-        
-        this._forma = nuevaForma;
-    }
-
-    rotarIzquierda(): void {
-        const n = this._forma.length;
-        const nuevaForma: number[][] = [];
-
-        for (let columna = 0; columna < n; columna++) {
-            nuevaForma.push([]);
-            for (let fila = 0; fila < n; fila++) {
-                nuevaForma[columna][fila] = this._forma[fila][n - 1 - columna];
-            }
-        }
-        
-        this._forma = nuevaForma;
     }
 }
