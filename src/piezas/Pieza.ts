ea974802a1883453b1cb5a5a7e282b48f40712b3
Fabricio Moyano <fabricx150@gmail.com>
2026-08-31 17:28:04 -0300

    Se movio la logica de rotacion a Pieza.ts

--- src/piezas/Pieza.ts
+++ src/piezas/Pieza.ts
@@ -1,22 +1,52 @@
 import { IRotador } from "../IRotador";
 
 export abstract class Pieza implements IRotador {
-    protected _x: number = 3;
-    protected _y: number = 0;
+    protected _columna: number = 3;
+    protected _fila: number = 0;
+
     constructor(
         protected _nombre: string,
-        protected _forma: number[][] = [],
+        protected _forma: number[][]
     ) {}
 
     get forma() { return this._forma; }
     get nombre() { return this._nombre; }
-    get x() { return this._x; }
-    get y() { return this._y; }
+    get columna() { return this._columna; }
+    get fila() { return this._fila; }
 
-    moverAbajo() { this._y++; }
-    moverIzquierda() { this._x--; }
-    moverDerecha() { this._x++; }
+    moverAbajo(): void { this._fila++; }
+    moverIzquierda(): void { this._columna--; }
+    moverDerecha(): void { this._columna++; }
 
-    abstract rotarIzquierda(): void;
-    abstract rotarDerecha(): void;
+    rotarDerecha(): void {
+        const tamaño = this._forma.length;
+        const nuevaForma: number[][] = [];
+
+        for (let fila = 0; fila < tamaño; fila++) {
+            nuevaForma.push([]);
+
+            for (let columna = 0; columna < tamaño; columna++) {
+                nuevaForma[fila][columna] =
+                    this._forma[tamaño - 1 - columna][fila];
+            }
+        }
+
+        this._forma = nuevaForma;
+    }
+
+    rotarIzquierda(): void {
+        const tamaño = this._forma.length;
+        const nuevaForma: number[][] = [];
+
+        for (let fila = 0; fila < tamaño; fila++) {
+            nuevaForma.push([]);
+
+            for (let columna = 0; columna < tamaño; columna++) {
+                nuevaForma[fila][columna] =
+                    this._forma[columna][tamaño - 1 - fila];
+            }
+        }
+
+        this._forma = nuevaForma;
+    }
 }
