01918523486079011fec6728f9a15a33baf4097d
amazingmanteca <caminosgerardoariel2016@gmail.com>
2026-08-31 17:12:14 -0300

    Se le añadio toda la funcionalidad al reloj

--- src/Reloj.ts
+++ src/Reloj.ts
@@ -1,7 +1,30 @@
 export class Reloj {
-    private timerId: ReturnType<typeof setInterval> | undefined;
+    private _timerId: ReturnType<typeof setInterval> | undefined;
     constructor(
         private _intervaloMs: number = 1000,
         private _onTick: () => void
     ) {}
+    iniciar(): void {
+        this.detener(); 
+        
+        this._timerId = setInterval(() => {
+            this._onTick();
+        }, this._intervaloMs);
+    }
+
+    detener(): void {
+        clearInterval(this._timerId);
+        this._timerId = undefined;
+    }
+
+    cambiarVelocidad(nuevoIntervaloMs: number): void {
+        const estabaCorriendo = this._timerId !== undefined;
+        
+        this._intervaloMs = nuevoIntervaloMs;
+        this.detener();
+
+        if (estabaCorriendo) {
+            this.iniciar();
+        }
+    }
 }
